16841aba3860ba2d579d2b0505e6f79822acd1f3
Rob Mayhew <rob@robmayhew.com>
2026-01-31 06:14:17 -0700

    Ping

--- godot/title.gd
+++ godot/title.gd
@@ -1,9 +1,11 @@
 extends Control
 
+# Delay to avoid game taps from restating a new game too soon
+var ignore_start = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass 
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -12,5 +14,32 @@ func _process(delta: float) -> void:
 
 
 func _on_button_pressed() -> void:
-	print("Start Clicked")
-	pass # Replace with function body.
+	ignore_start = false
+	start_game()
+	pass
+
+# Handle input for spacebar and tap/click
+func _input(event: InputEvent) -> void:
+	# Check for spacebar press
+	if event is InputEventKey:
+		if event.pressed and event.keycode == KEY_SPACE:
+			start_game()
+
+	# Check for mouse click or touch tap
+	if event is InputEventMouseButton:
+		if event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+			start_game()
+
+	if event is InputEventScreenTouch:
+		if event.pressed:
+			start_game()
+			
+
+func start_game() -> void:
+	if ignore_start:
+		return
+	SceneManager.change_scene("res://game.tscn");	
+
+func _on_wait_to_start_timer_timeout() -> void:
+	ignore_start = false
+	pass
